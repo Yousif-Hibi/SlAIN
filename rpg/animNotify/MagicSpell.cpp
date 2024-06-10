0c8017afc1cc8a4712dab40c18999a8baf60dbcf
Yousif-Hibi <95185875+Yousif-Hibi@users.noreply.github.com>
2024-06-10 10:11:42 +0300

    update  animNotify

--- rpg/animNotify/MagicSpell.cpp
+++ rpg/animNotify/MagicSpell.cpp
@@ -5,6 +5,7 @@
 #include <rpg/SoulsLikeCharacter.h>
 #include "rpg/Actors/C_BaseMagicWeapon.h"
 #include "rpg/Component/C_CombatComponent.h"
+#include "rpg/AI/C_MasterAI.h"
 
 
 void  UMagicSpell::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
@@ -20,7 +21,7 @@ bool  UMagicSpell::Received_Notify(USkeletalMeshComponent* MeshComp, UAnimSequen
 
 
 	if (actor) {
-		ASoulsLikeCharacter* MainCharacter = Cast<ASoulsLikeCharacter>(actor);
+		auto* MainCharacter = Cast<AC_MasterAI>(actor);
 		if (MainCharacter) {
 			AC_BaseMagicWeapon* magicGlove = Cast<AC_BaseMagicWeapon>(MainCharacter->CombatComponent->GetMainWeapon());
 			if (magicGlove)
