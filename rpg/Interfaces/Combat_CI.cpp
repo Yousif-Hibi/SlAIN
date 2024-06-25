6b15ad05c4e2c779bf295f8705edb724f39c4ba5
Yousif-Hibi <yousif.hibi@gmail.com>
2024-06-25 10:47:10 +0300

    removing duplicate files

--- rpg/Interfaces/Combat_CI.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Combat_CI.h"
-
-// Add default functionality here for any ICombat_CI functions that are not pure virtual.
--- rpg/Interfaces/Interactive_CI.cpp
+++
@@ -1,7 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Interactive_CI.h"
-
-// Add default functionality here for any IInteractive_CI functions that are not pure virtual.
-
